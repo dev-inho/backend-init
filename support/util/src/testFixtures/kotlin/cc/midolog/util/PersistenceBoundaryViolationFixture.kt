65f75066b6cd2965d2e008c93665be369e9d06eb
Orca Worker <worker@midolog.cc>
2026-09-13 00:42:03 +0900

    docs: synchronize persistence boundary documentation and remove test fixture

--- support/util/src/testFixtures/kotlin/cc/midolog/util/PersistenceBoundaryViolationFixture.kt
+++
@@ -1,9 +0,0 @@
-package cc.midolog.util
-
-import cc.midolog.storage.jpa.config.JpaStorageConfig
-
-/**
- * 가드 동작 입증용 임시 위반 픽스처.
- * PersistenceBoundaryTest에서 금지된 import가 감지되어 빨간색(실패)이 되는지 확인하기 위한 용도.
- */
-class PersistenceBoundaryViolationFixture
